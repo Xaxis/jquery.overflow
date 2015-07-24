72a491b812ed8c4e5ac3ab198c0a854caf946b6e
Xaxis <william.neeley@gmail.com>
2015-07-24 14:38:50 -0700

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,87 @@
 # jquery.overflow
-A jQuery plugin for responding to elements that have overflowed off the viewport.
+
+Version 1.0.0
+
+## Summary
+
+A jQuery plugin for responding to elements that have overflowed the viewport.
+
+jQuery.overflow allows you to fire two separate callbacks. One callback (`over`) which fires continuously when an 
+element is outside of the viewport (fully or partially) and another (`under`) that fires once when an overflowed 
+element is no longer overflowed from a direction (top, right, bottom, or left).
+
+A practical usage example might be keeping a popup message box visible when a browser window is being resized.
+
+## Author
+
+Wil Neeley ( [@wilneeley](http://twitter.com/wilneeley) / [github.com](https://github.com/Xaxis) )
+
+## Usage
+
+Include `jquery.overflow.min.js` after jQuery.
+
+### React to Overflowing Elements
+
+The following example repositions an element based on the direction of its overflow. In this instance a message box 
+overflows on the right side of the screen and is then repositioned above its target. When the message box would no 
+longer overflow (upon continued screen resize) it is placed back where it started (using 
+[jQuery.boxel](https://github.com/Xaxis/jquery.boxel)).
+
+```css
+#rel {
+    width: 400px;
+    height: 260px;
+    float: right;
+}
+.box {
+    width: 200px;
+    border: 1px dashed black;
+    position: absolute;
+}
+```
+
+```html
+<button id="rel">relative element</button>
+<div id="box" class="box">message box</div>
+```
+
+```javascript
+    var box = $('#box');
+    var rel = $('#rel');
+
+    // Position element using jQuery.boxel
+    box.boxel({pos: 'r', rel: rel});
+    $(window).on('resize', function() {
+        box.boxel({pos: 'r', rel: rel});
+    });
+
+    // jQuery.overflow demo
+    $('.box').overflow({
+        load: true,
+        over: function(dir, dist) {
+            if (dir == 'right') {
+                box.boxel({pos: 't', rel: rel});
+            }
+        },
+        under: function(dir, dist) {
+            if (dir == 'right') {
+                box.boxel({pos: 'r', rel: rel});
+            }
+        }
+    });
+```
+
+One other property `load` passed to jQuery.overflow determines if the `over` and `under` callbacks are to fire once on
+load. By default `load` is `true`.
+
+For a clearer example see `test/test.html`.
+
+## Examples
+
+See `test/test.html`.
+
+## Changelog
+
+### Version 1.0.0
+
+* initial release
---
+++ src/jquery.overflow.js
@@ -0,0 +1,131 @@
+/**
+ * jQuery.overflow
+ *
+ * (a) Wil Neeley
+ * (c) Code may be freely distributed under the MIT license.
+ */
+;(function ( $, window, document, undefined ) {
+
+  "use strict";
+
+  var
+    plugin_name   = 'overflow',
+    defaults      = {
+      load:       true,
+      over:       function(){},
+      under:      function(){}
+    };
+
+  // Plugin constructor
+  function Plugin( element, options ) {
+    this._name = plugin_name;
+    this._defaults = defaults;
+    this.element = element;
+    this.options = $.extend({}, defaults, options);
+    this.init();
+  }
+
+  // Extend plugin prototype
+  $.extend(Plugin.prototype, {
+
+    // Initialization method
+    init: function() {
+      var
+        self      = this;
+      if (this.options.load) this.watchForOverflows();
+      $(window).on('resize', function() {
+        self.watchForOverflows.call(self);
+      });
+    },
+
+    /**
+     * Triggers appropriate callbacks on elements upon overflow/underflow conditions.
+     */
+    watchForOverflows: function() {
+      var
+        self        = this,
+        elms        = $(this.element);
+
+      // Iterate over selected elements
+      elms.each(function(idx, elm) {
+        var
+          $elm        = $(elm),
+          e_offs      = $elm.offset(),
+          e_y         = e_offs.top,
+          e_x         = e_offs.left,
+          e_h         = $elm.outerHeight(),
+          e_w         = $elm.outerWidth(),
+          w_h         = $(window).outerHeight(),
+          w_w         = $(window).outerWidth(),
+          dir         = false,
+          dist        = 0;
+
+        // Overflows right
+        if (w_w < (e_x + e_w)) {
+          dir = 'right';
+          dist = (e_x + e_w) - w_w;
+          self.options.over(dir, dist);
+          $elm.data(dir, true);
+        }
+        else if ($elm.data('right')) {
+          dir = 'right';
+          dist = (e_x + e_w) - w_w;
+          $elm.data(dir, false);
+          self.options.under(dir, dist);
+        }
+
+        // Overflows left
+        if (e_x < 0) {
+          dir = 'left';
+          dist = e_x;
+          self.options.over(dir, dist);
+          $elm.data(dir, true);
+        }
+        else if ($elm.data('left')) {
+          dir = 'left';
+          dist = e_x;
+          $elm.data(dir, false);
+          self.options.under(dir, dist);
+        }
+
+        // Overflows top
+        if (e_y < 0) {
+          dir = 'top';
+          dist = e_y;
+          self.options.over(dir, dist);
+          $elm.data(dir, true);
+        }
+        else if ($elm.data('top')) {
+          dir = 'top';
+          dist = e_x;
+          $elm.data(dir, false);
+          self.options.under(dir, dist);
+        }
+
+        // Overflows bottom
+        if (w_h < (e_y + e_h)) {
+          dir = 'bottom';
+          dist = (e_y + e_h) - w_h;
+          self.options.over(dir, dist);
+          $elm.data(dir, true);
+        }
+        else if ($elm.data('bottom')) {
+          dir = 'bottom';
+          dist = e_x;
+          $elm.data(dir, false);
+          self.options.under(dir, dist);
+        }
+      });
+    }
+  });
+
+  // Plugin wrapper
+  $.fn[plugin_name] = function ( options ) {
+    return this.each(function () {
+      if (!$.data(this, 'plugin_' + plugin_name)) {
+        $.data(this, 'plugin_' + plugin_name, new Plugin( this, options ));
+      }
+    });
+  };
+
+})( jQuery, window, document );
